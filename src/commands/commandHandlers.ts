d441555f947c7d374f7854c64f69262162290a33
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-01-31 12:15:11 +0200

    fix: module and block-scope errors

--- src/commands/commandHandlers.ts
+++ src/commands/commandHandlers.ts
@@ -1,7 +1,7 @@
 //packages
-import path from "path";
-import { cwd } from "process";
+const path = require("path");
 const fs = require('fs');
+const process = require("process");
 const Table = require('cli-table');
 const processDirectory = process.cwd();
 const programPath = `${path.join(__dirname, '..')}`;
