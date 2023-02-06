e29301a980cf6a027b4572ed4fbe0f118482446d
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-02-06 12:23:47 +0200

    refractor: run prettier

--- bin/npx-proj.ts
+++ bin/npx-proj.ts
@@ -1,58 +1,85 @@
 #!/usr/bin/env node
 
-const  { Command }  = require('commander');
-const { cwd } = require('process');
-const  path  = require("path");
+const { Command } = require("commander");
+const { cwd } = require("process");
+const path = require("path");
 
 const program = new Command();
 
 //commandHandlers
-import { init, generate, descriptionManager, descriptionDisplayer } from '../src/commands/commandHandlers';
+import {
+  init,
+  generate,
+  descriptionManager,
+  descriptionDisplayer,
+} from "../src/commands/commandHandlers";
 
 program
-  .name('npx-proj')
-  .description('Node.js project-structure manager, developed by pacifiquem@github.com \n\n')
+  .name("npx-proj")
+  .description(
+    "Node.js project-structure manager, developed by pacifiquem@github.com \n\n"
+  )
   .usage("command [options]")
-  .version(`\x1b[1mv${require('../package.json').version}\x1b[0m`, '-v, --version', 'Output the current version.')
-  .helpOption('-h, --help', 'Output usage information.');
+  .version(
+    `\x1b[1mv${require("../package.json").version}\x1b[0m`,
+    "-v, --version",
+    "Output the current version."
+  )
+  .helpOption("-h, --help", "Output usage information.");
 
+program
+  .command("init")
+  .description("Command to initialize the project .")
+  .argument("<string>", "name of your project")
+  .option("-fo, --folders <value...>", "folder you want to initialize ?", "")
+  .action(
+    (argument: string, options: { files: string[]; folders: string[] }) => {
+      let directory = cwd();
+      init(argument, options.folders, directory);
+    }
+  );
 
-program.command('init')
-  .description('Command to initialize the project .')
-  .argument('<string>', 'name of your project')
-  .option('-fo, --folders <value...>', 'folder you want to initialize ?', '')
-  .action((argument: string, options: { files: string[], folders: string[] }) => {
-    let directory = cwd();
-    init(argument, options.folders, directory);
-  });
-
-program.command('generate')
-  .description('Command to generate any file in a given folder .')
-  .argument('<string>', 'folder file will be generated into .')
-  .option('-fi, --files <value...>', 'file you want to generate ?')
+program
+  .command("generate")
+  .description("Command to generate any file in a given folder .")
+  .argument("<string>", "folder file will be generated into .")
+  .option("-fi, --files <value...>", "file you want to generate ?")
   .action((argument: string, options: { files: string[] }) => {
-
     let directory = cwd();
     directory = path.join(directory, argument);
     generate(options.files, directory);
-
   });
 
-program.command('add-description')
-  .description('Command to add description to your project And its sub folders .')
-  .option('-p, --project <string>', 'project you want to add description to ?')
-  .option('-fo, --folder <string>', 'folder you want to add description to ?')
-  .option('-fi, --file <string>', 'file you want to add description to ?')
-  .option('-m, --message <string>', 'message you want to add to your description ?')
+program
+  .command("add-description")
+  .description(
+    "Command to add description to your project And its sub folders ."
+  )
+  .option("-p, --project <string>", "project you want to add description to ?")
+  .option("-fo, --folder <string>", "folder you want to add description to ?")
+  .option("-fi, --file <string>", "file you want to add description to ?")
+  .option(
+    "-m, --message <string>",
+    "message you want to add to your description ?"
+  )
   .action((argument: any, options: any) => {
     descriptionManager(argument);
   });
 
-program.command('describe')
-  .description('Command to display description of your project And its sub folders .')
-  .option('-p, --project <string>', 'project you want to display description of ?')
-  .option('-fo, --folder <string>', 'folder you want to display description of ?')
-  .option('-fi, --file <string>', 'file you want to display description of ?')
+program
+  .command("describe")
+  .description(
+    "Command to display description of your project And its sub folders ."
+  )
+  .option(
+    "-p, --project <string>",
+    "project you want to display description of ?"
+  )
+  .option(
+    "-fo, --folder <string>",
+    "folder you want to display description of ?"
+  )
+  .option("-fi, --file <string>", "file you want to display description of ?")
   .action((argument: string, options: any) => {
     descriptionDisplayer(argument);
   });
