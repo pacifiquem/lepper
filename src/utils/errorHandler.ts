a07c3c2df06e98c0c7f6e67d100d6f565d607d25
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-03-27 13:30:35 +0200

    feat: projectNotFoundError added

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,6 +1,13 @@
 module.exports.MissingArgumentsErrror = class MissingArgumentsErrror extends Error {
-    constructor(missing_args: string) {
-        super(`MissingArguments ${missing_args}`);
+    constructor(missing_args: string[]) {
+        super(`MissingArguments: ${missing_args}`);
         this.name =   "MissingArgumentsErrror";
+    }
+}
+
+module.exports.ProjectNotFoundErrror = class ProjectNotFoundErrror extends Error {
+    constructor(missing_project: string) {
+        super(`MissingProject: ${missing_project}`);
+        this.name =   "ProjectNotFoundErrror";
     }
 }
